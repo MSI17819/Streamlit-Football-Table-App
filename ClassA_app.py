4d2439728cb8ca4b111a4a646ba222a721472dd8
MSI17819 <100449395+MSI17819@users.noreply.github.com>
2023-11-21 17:40:13 +0100

    Add files via upload

--- ClassA_app.py
+++ ClassA_app.py
@@ -91,7 +91,7 @@ with c2:
 
 # Open json file from github url
 with urllib.request.urlopen(r'https://raw.githubusercontent.com/MSI17819/Streamlit-Football-Table-App/main/ClassA_result_after_12.json') as url:
-    data_after_8 = json.load(url)
+    data_after_6 = json.load(url)
 
 # Display of notes to the chart
 st.markdown("""The chart shows the position and number of matches played for each team.""")
@@ -140,7 +140,7 @@ if st.button('Chart'):
     fig, ax = bumpy.plot(
         x_list=match_day,  # match-day or match-week
         y_list=np.linspace(1, 14, 14).astype(int),  # position value from 1 to 20
-        values=data_after_8,  # values having positions for each team
+        values=data_after_6,  # values having positions for each team
         secondary_alpha=0.4,   # alpha value for non-shaded lines/markers
         highlight_dict=highlight_dict,  # team to be highlighted with their colors
         figsize=(18, 8),  # size of the figure
